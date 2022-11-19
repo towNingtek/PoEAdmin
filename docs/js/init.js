83af970f764dd07453640328e13720f47685543b
yillkid <yillkid@gmail.com>
2022-11-20 00:55:50 +0800

    feat: add dashboard init function

--- docs/js/init.js
+++ docs/js/init.js
@@ -9,6 +9,21 @@ function init() {
     var list_txns = txn_read();
     alert(JSON.stringify(list_txns));
 
+    var obj_txn_tbody = document.getElementById("txn_tbody");
+    for (var index=0; index<list_txns.length; index++) {
+      console.log(list_txns[index]);
+    }
+    
+    /*
+    <tr>
+        <td>000001</td>
+        <td>2022/03/19 00:12</td>
+        <td>N7Q4PR...</td>
+        <td>1JdZnHiS...</td>
+        <td>Model A</td>
+        <td></td>
+    </tr>
+    */
   }
 
 }
