3213b95c54d3dc8dc3802420fc05299bd685882a
yillkid <yillkid@gmail.com>
2022-11-20 15:03:54 +0800

    feat: add dashboard init function

--- docs/js/init.js
+++ docs/js/init.js
@@ -1,3 +1,5 @@
+const { doc } = require("prettier");
+
 function init() {
   // Get path
   var path = window.location.pathname;
@@ -14,7 +16,31 @@ function init() {
       var obj_txn = JSON.parse(list_txns[index]); 
       console.log(JSON.stringify(obj_txn));
     }
-    
+
+    var obj_tr = document.createElement("tr");
+    var obj_td_id = document.createElement("td");
+    var obj_td_timestamp = document.createElement("td");
+    var obj_td_txn_hash = document.createElement("td");
+    var obj_td_sig = document.createElement("td");
+    var obj_td_tag = document.createElement("td");
+    var obj_td_none = document.createElement("td");
+
+    obj_td_id.innerHTML = "1";
+    obj_td_timestamp.innerHTML = "1";
+    obj_td_txn_hash.innerHTML = "1";
+    obj_td_sig.innerHTML = "1";
+    obj_td_tag.innerHTML = "1";
+    obj_td_none.innerHTML = "1";
+
+    obj_tr.append(obj_td_id);
+    obj_tr.append(obj_td_timestamp);
+    obj_tr.append(obj_td_txn_hash);
+    obj_tr.append(obj_td_sig);
+    obj_tr.append(obj_td_tag);
+    obj_tr.append(obj_td_none);
+
+    obj_txn_tbody.append(obj_tr);
+
     /*
     <tr>
         <td>000001</td>
