5eca8c95315026e34b547415954dd74eaf8912d0
yillkid <yillkid@gmail.com>
2022-11-20 15:26:04 +0800

    feat: add dashboard init function

--- docs/js/init.js
+++ docs/js/init.js
@@ -13,32 +13,31 @@ function init() {
     var obj_txn_tbody = document.getElementById("txn_tbody");
     for (var index=0; index<list_txns.length; index++) {
       console.log(list_txns[index].hash);
+
+      var obj_tr = document.createElement("tr");
+      var obj_td_id = document.createElement("td");
+      var obj_td_timestamp = document.createElement("td");
+      var obj_td_txn_hash = document.createElement("td");
+      var obj_td_sig = document.createElement("td");
+      var obj_td_tag = document.createElement("td");
+      var obj_td_none = document.createElement("td");
+
+      obj_td_id.innerHTML = "1";
+      obj_td_timestamp.innerHTML = "1";
+      obj_td_txn_hash.innerHTML = "1";
+      obj_td_sig.innerHTML = "1";
+      obj_td_tag.innerHTML = "1";
+      obj_td_none.innerHTML = "1";
+
+      obj_tr.append(obj_td_id);
+      obj_tr.append(obj_td_timestamp);
+      obj_tr.append(obj_td_txn_hash);
+      obj_tr.append(obj_td_sig);
+      obj_tr.append(obj_td_tag);
+      obj_tr.append(obj_td_none);
+
+      obj_txn_tbody.append(obj_tr);
     }
-
-    var obj_tr = document.createElement("tr");
-    var obj_td_id = document.createElement("td");
-    var obj_td_timestamp = document.createElement("td");
-    var obj_td_txn_hash = document.createElement("td");
-    var obj_td_sig = document.createElement("td");
-    var obj_td_tag = document.createElement("td");
-    var obj_td_none = document.createElement("td");
-
-    obj_td_id.innerHTML = "1";
-    obj_td_timestamp.innerHTML = "1";
-    obj_td_txn_hash.innerHTML = "1";
-    obj_td_sig.innerHTML = "1";
-    obj_td_tag.innerHTML = "1";
-    obj_td_none.innerHTML = "1";
-
-    obj_tr.append(obj_td_id);
-    obj_tr.append(obj_td_timestamp);
-    obj_tr.append(obj_td_txn_hash);
-    obj_tr.append(obj_td_sig);
-    obj_tr.append(obj_td_tag);
-    obj_tr.append(obj_td_none);
-
-    obj_txn_tbody.append(obj_tr);
-
     /*
     <tr>
         <td>000001</td>
