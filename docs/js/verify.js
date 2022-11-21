c4863e38de53416623986b4ebb477e92fb4f979c
yillkid <yillkid@gmail.com>
2022-11-21 22:13:08 +0800

    feat: add dashboard init function

---
+++ docs/js/verify.js
@@ -0,0 +1,33 @@
+function txn_verify() {
+  var dataJSON = {};
+  var resultData = {};
+
+  $.ajax({
+    url: "https://geo.townway.com.tw/txn/verify",
+    type: "POST",
+    async: false,
+    crossDomain: true,
+    contentType: "application/json",
+    data: JSON.stringify(dataJSON),
+    success: function(returnData) {
+      // const obj = JSON.parse(returnData);
+      resultData = returnData;
+    },
+      error: function(xhr, ajaxOptions, thrownError){
+      console.log(thrownError);
+    }
+  });
+  return resultData;
+}
+
+$(function () {
+    $("form").on("submit", function(e){
+      e.preventDefault();
+      
+      /* var tag = document.getElementById("TransactionTag").value;
+      var msg = document.getElementById("TransactionMsg").value; */
+
+      var response = txn_verify();
+      document.getElementById("verifyResponse").innerHTML = response;
+    });
+});
